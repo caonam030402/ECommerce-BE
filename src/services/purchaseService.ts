415fd47fb8e7f46b5e6de7479e807d135a11c60e
Cao Nam <nam52871@donga.edu.vn>
2023-05-15 08:43:18 +0700

    Fix add to cart #2

--- src/services/purchaseService.ts
+++ src/services/purchaseService.ts
@@ -1,6 +1,7 @@
 import { Product } from '~/models/productModel'
 import { Purchase } from '~/models/purchaseModel'
 import { IProduct } from '~/types/productType'
+import { IPurchase } from '~/types/purchaseType'
 import { IUser } from '~/types/userType'
 
 export interface IRequest extends Request {
@@ -10,26 +11,26 @@ export interface IRequest extends Request {
 const purchaseService = {
   addToCart: async ({ product_id, buy_count }: { product_id: string; buy_count: number }, user: IUser) => {
     const product = (await Product.findById(product_id)) as IProduct
-    const purchase = await Purchase.create({ buy_count, product, user })
+    const purchase = await Purchase.findOne({ user: user?._id, product: product._id })
 
-    const totalBuyCount = await Purchase.aggregate([
-      {
-        $group: {
-          _id: null,
-          total: { $sum: '$buy_count' }
-        }
-      }
-    ]).exec()
-
-    return {
-      _id: purchase._id,
-      buy_count: totalBuyCount[0].total,
-      price: product.price,
-      price_before_discount: product.price_before_discount,
-      status: -1,
-      user: user?._id,
-      product
+    const purchaseArray: IPurchase[] = []
+    if (purchase) {
+      purchase.buy_count += buy_count
+      const purchaseSave = await purchase.save()
+      purchaseArray.push(purchaseSave)
+    } else {
+      const newPurchase: IPurchase = await Purchase.create({
+        buy_count: buy_count,
+        price: product.price,
+        price_before_discount: product.price_before_discount,
+        status: -1,
+        user: user?._id,
+        product: product._id
+      })
+      purchaseArray.push(newPurchase)
     }
+
+    return purchaseArray[0]
   }
 }
 export default purchaseService
