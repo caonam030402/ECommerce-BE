1804f6d59886f8e42691a63a3c160855199fb19a
Cao Nam <nam52871@donga.edu.vn>
2023-05-15 07:07:11 +0700

    Fix add to cart

--- src/services/purchaseService.ts
+++ src/services/purchaseService.ts
@@ -1,8 +1,6 @@
-import { ObjectId } from 'mongoose'
 import { Product } from '~/models/productModel'
 import { Purchase } from '~/models/purchaseModel'
 import { IProduct } from '~/types/productType'
-import { IPurchase } from '~/types/purchaseType'
 import { IUser } from '~/types/userType'
 
 export interface IRequest extends Request {
@@ -13,9 +11,19 @@ const purchaseService = {
   addToCart: async ({ product_id, buy_count }: { product_id: string; buy_count: number }, user: IUser) => {
     const product = (await Product.findById(product_id)) as IProduct
     const purchase = await Purchase.create({ buy_count, product, user })
+
+    const totalBuyCount = await Purchase.aggregate([
+      {
+        $group: {
+          _id: null,
+          total: { $sum: '$buy_count' }
+        }
+      }
+    ]).exec()
+
     return {
       _id: purchase._id,
-      buy_count: purchase.buy_count,
+      buy_count: totalBuyCount[0].total,
       price: product.price,
       price_before_discount: product.price_before_discount,
       status: -1,
